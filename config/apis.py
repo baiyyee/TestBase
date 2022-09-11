fa407f0c41aaa274a560b7b56491e22342e15fb4
Huabo He <hhbstar@hotmail.com>
2022-09-11 16:54:44 +0800

    feat: add api sets for api test

---
+++ config/apis.py
@@ -0,0 +1,14 @@
+# 用户授权
+LOGIN = "/login"
+
+
+# 用户管理
+USER = "/user"
+USER_SPECIFIC = "/user/{id}"
+USER_RESET_PWD = "/user/{id}/password/reset"
+USER_PROFILE = "/user/profile"
+
+
+# 文件管理
+FILE = "/file"
+FILE_UPLOAD = "/file/upload"
