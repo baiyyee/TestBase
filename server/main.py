ddfaf9b6838aad39ab84d8cca75ee5dbddf6c169
Huabo He <hhbstar@hotmail.com>
2022-09-11 16:58:41 +0800

    feat: add swagger doc meta data and exception handler and app start up event

--- server/main.py
+++ server/main.py
@@ -1,53 +1,75 @@
 from models import User
 from dependencies.auth import CXT
 from routers import auth, user, file
-from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request, status
 from fastapi.exceptions import RequestValidationError
-from dependencies.database import engine, SQLModel, Session
+from dependencies.database import SQLModel, Session, engine
+from sqlalchemy.exc import IntegrityError as SqlAlchemyIntegrityError
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
 
 description = "API Demo Is Used For API Testing. 🚀"
 
-app = FastAPI(docs_url="/", title="API Demo", description=description, version="0.0.1")
+app = FastAPI(
+    docs_url="/",
+    title="API Demo",
+    description=description,
+    version="0.0.1",
+    contact={
+        "name": "Huabo He",
+        "url": "https://baiyyee.github.io/",
+        "email": "hhbstar@hotmail.com",
+    },
+    license_info={
+        "name": "MIT",
+        "url": "https://mit-license.org/",
+    },
+)
+
+
+@app.exception_handler(SqlAlchemyIntegrityError)
+async def sqlalchemy_exception_handler(request, exc):
+
+    message = {error.split(".")[-1]: error.split(":")[0] for error in exc.args}
+    return JSONResponse(content={"error": message}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
 @app.exception_handler(StarletteHTTPException)
 async def http_exception_handler(request, exc):
 
-    return JSONResponse({"message": exc.detail}, status_code=exc.status_code)
+    return JSONResponse(content={"error": exc.detail}, status_code=exc.status_code)
 
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
 
-    error_message = {}
-    for error in exc.errors():
-        field = error.get("loc")[-1]
-        error_message[field] = [{"code": error.get("type"), "message": error.get("msg")}]
-
-    return JSONResponse({"message": "Field Invalid", "fields": error_message})
+    message = {error.get("loc")[-1]: error.get("msg") for error in exc.errors()}
+    return JSONResponse(content={"error": message}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
 @app.on_event("startup")
 async def init_db():
+
+    # Clean Up DB Table
+    SQLModel.metadata.drop_all(engine)
 
     # Init DB Table
     SQLModel.metadata.create_all(engine)
 
     # Init User
     with Session(engine) as session:
-        user = User(name="root", email="root@test.com", role=0, status=1, password=CXT.hash("123456"), creator=1)
+        user = User(name="root", email="root@test.com", role=0, status=1, creator=1)
+        user.password = CXT.hash("123456")
         session.add(user)
         session.commit()
 
 
-@app.on_event("shutdown")
-async def clean_db():
+# @app.on_event("shutdown")
+# async def clean_db():
 
-    # Clean Up DB Table
-    SQLModel.metadata.drop_all(engine)
+#     # Clean Up DB Table
+#     SQLModel.metadata.drop_all(engine)
 
 
 app.include_router(auth.router)
